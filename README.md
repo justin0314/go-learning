892c8245081c13a74ffd8b12022740389e5adbb9
justin0314 <1905861549@qq.com>
2024-06-25 12:45:05 +0800

    golang学习：创建模块

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # go-learning
 一个关于golang初学者的仓库
+## 1、创建模块
++ go mod init 模块名称：为模块创建go.mod
++ 大写字母开头的函数可以被不在同一包中的函数调用
++ := 是一种快捷的声明并初始化变量的方式
++ 声明变量使用 var message string
+## 2、错误处理
++ 抛出异常：errors.New("empty name")
++ 打印异常：log.Fatal(err)
+## 3、切片
++ 为切片赋值：[]string={}
++ len(xxx)
++ 创建切片：make()
+## 4、测试
+
+
--- hello.go
+++ hello.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 import "rsc.io/quote"
 
 func main() {
+	// 设置预定义Logger的属性，包括
+	// 日志条目前缀和禁用打印的标志
+	//  时间、源文件和行号.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	fmt.Println(quote.Go())
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := Hellos(names)
+
+	// message, err := Hello("justin")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
 }
